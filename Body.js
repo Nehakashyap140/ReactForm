6263e2feea2bc50961a4a708050e38ddaf7a94c9
Neha kashyap <97278736+Nehakashyap140@users.noreply.github.com>
2022-04-26 17:44:37 +0530

    Add files via upload

---
+++ Body.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import "./Body.css";
+
+
+export default function Body()
+{
+    return(
+    <>
+        <div className="a1">
+            <h1>Saathi</h1>
+            <div className="a2">
+            <input type="text" placeholder="Enter Your Full name"/>
+            <input type="text" placeholder="Enter your Mobile No."/>
+            <input type="text" placeholder=" Enter your Email Id"/>
+            <input type="text" placeholder="Reference Code"/>
+            <input type="text" placeholder="Enter your Pincode"/>
+            <button>Submit</button>
+            </div> 
+        </div>
+    </>
+    )
+};
+ 
